cf6b27a9a9b0292b9aaf9e6637b3c092a385f95c
rossie111 <78100554+rossie111@users.noreply.github.com>
2023-03-06 21:54:09 -0800

    Blocked out alert
    with artist statement

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-	alert( 'My name is rossie.')
-});
+// $(document).ready(function(){
+// 	alert( 'My name is rossie.');
+// 	alert( 'I am an artist, musician, and community organizer that has been living, learning, and playing on Yelamu land (now referred to as San Francisco) for ~10 years. I grew up 20 miles south of SF and am deeply indebted to the rolling hills and dreams of a progressive utopia that have shaped my understanding of a lived environment. ')
+// 	alert( 'I draw heavily from my background as a food sovereignty organizer, a scholar and former research assistant in the field of Geography, a former freelance photojournalist, and a mestiza (mixed native american and european descent) queer.');
+	
+// });
 
-console.log('hi'); 
+// console.log('hi'); 
